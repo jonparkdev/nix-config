310708fa00a07e5aadb54b20e06fcf200bccf0c8
Jon Park <accounts@jonpark.dev>
2025-05-21 15:45:35 -0600

    feat: initial commit with darwin

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+    description = "Jon's NixOS Configuration";
+
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+
+        home-manager = {
+            url = "github:nix-community/home-manager/release-24.11";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+
+        darwin = {
+            url = "github:nix-darwin/nix-darwin/nix-darwin-24.11";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+    };
+
+    outputs = {
+        self,
+        nixpkgs,
+        home-manager,
+        ...
+    } @ inputs: let
+        user = "jon";
+        darwinSystems = [ "aarch64-darwin" ];
+    in {
+        darwinConfigurations = nixpkgs.lib.genAttrs darwinSystems (system:
+            darwin.lib.darwinSystem {
+                inherit system;
+                specialArgs = inputs;
+                modules = [
+                    home-manager.darwinModules.home-manager
+                    ./hosts/darwin
+                ];
+            }
+        );
+    }
+}
---
+++ home-manager/home.nix
@@ -0,0 +1,29 @@
+# This is your home-manager configuration file
+# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
+{
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
+  home = {
+    username = "jonpark";
+    homeDirectory = "/home/jonpark";
+  };
+
+  # Add stuff for your user as you see fit:
+  # programs.neovim.enable = true;
+  # home.packages = with pkgs; [ steam ];
+
+  # Enable home-manager and git
+  programs.home-manager.enable = true;
+  programs.git.enable = true;
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "23.05";
+}
