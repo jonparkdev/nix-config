717d34d0a3a7f9220b7750bdb20c0bd9e5777cd0
Jonathan Park <accounts@jonpark.dev>
2025-09-18 15:50:51 -0600

    feat: it finally works

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -66,7 +66,7 @@ in
             enable = true;
             extraConfig = ''
               Host *
-                  IdentityAgent ~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock
+                  IdentityAgent "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
             '';
           };
 
