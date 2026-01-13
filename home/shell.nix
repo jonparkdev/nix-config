c279c1427382b1290466c126ed110383449e817f
Jonathan Park <accounts@jonpark.dev>
2026-01-13 16:47:48 -0700

    fix: starship icons

--- home/shell.nix
+++ home/shell.nix
@@ -32,30 +32,30 @@
         distro=$(awk '/^ID=/' /etc/*-release | awk -F'=' '{ print tolower($2) }')
         case $distro in
             *kali*)                  ICON="ﴣ";;
-            *arch*)                  ICON="";;
-            *debian*)                ICON="";;
-            *raspbian*)              ICON="";;
-            *ubuntu*)                ICON="";;
-            *elementary*)            ICON="";;
-            *fedora*)                ICON="";;
-            *coreos*)                ICON="";;
-            *gentoo*)                ICON="";;
-            *mageia*)                ICON="";;
-            *centos*)                ICON="";;
-            *opensuse*|*tumbleweed*) ICON="";;
-            *sabayon*)               ICON="";;
-            *slackware*)             ICON="";;
-            *linuxmint*)             ICON="";;
-            *alpine*)                ICON="";;
-            *aosc*)                  ICON="";;
-            *nixos*)                 ICON="";;
-            *devuan*)                ICON="";;
-            *manjaro*)               ICON="";;
-            *rhel*)                  ICON="";;
-            *)                       ICON="";;
+            *arch*)                  ICON="";;
+            *debian*)                ICON="";;
+            *raspbian*)              ICON="";;
+            *ubuntu*)                ICON="";;
+            *elementary*)            ICON="";;
+            *fedora*)                ICON="";;
+            *coreos*)                ICON="";;
+            *gentoo*)                ICON="";;
+            *mageia*)                ICON="";;
+            *centos*)                ICON="";;
+            *opensuse*|*tumbleweed*) ICON="";;
+            *sabayon*)               ICON="";;
+            *slackware*)             ICON="";;
+            *linuxmint*)             ICON="";;
+            *alpine*)                ICON="";;
+            *aosc*)                  ICON="";;
+            *nixos*)                 ICON="";;
+            *devuan*)                ICON="";;
+            *manjaro*)               ICON="";;
+            *rhel*)                  ICON="";;
+            *)                       ICON="";;
         esac
       else
-        ICON=""
+        ICON=""
       fi
       export STARSHIP_DISTRO="$ICON"
     '';
